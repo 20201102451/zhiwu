73ef56b69ae5004dd0eb7cca5360c7bd07149d76
Labio07 <1348757279@qq.com>
2023-12-01 19:07:06 +0800

    创建项目--后端

---
+++ src/main/java/com/example/zhiwu/ZhiwuApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhiwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiwuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiwuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zhiwu/ZhiwuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhiwu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhiwuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
