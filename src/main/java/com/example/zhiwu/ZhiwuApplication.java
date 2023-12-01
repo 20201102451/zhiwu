68779a3a7d86aa7c79ddfdaefb3263d85ad531b8
Labio07 <1348757279@qq.com>
2023-12-01 19:07:06 +0800

    创建项目--后端

--- src/main/java/com/example/zhiwu/ZhiwuApplication.java
+++ src/main/java/com/example/zhiwu/ZhiwuApplication.java
@@ -2,12 +2,18 @@ package com.example.zhiwu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ZhiwuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZhiwuApplication.class, args);
     }
-
+    @GetMapping("/")
+    public String index(){
+        return "ok!";
+    }
 }
